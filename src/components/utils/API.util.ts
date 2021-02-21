957b4286df8c9865bd006476ac326b6e94104072
Cigan <darknew1998@mail.ru>
2021-02-21 19:39:15 +0700

    Saga tests completed

--- src/components/utils/API.util.ts
+++ src/components/utils/API.util.ts
@@ -1,1 +1,44 @@
-export const API = () => {};
+import { IRadioStation } from '../../reducers/RadioWidgetReducer/RadioWidget.types';
+import Avatar from '../../static/images/RadioWidget/radioAvatar.png';
+
+export const mockApiData: Array<IRadioStation> = [
+    {
+        frequency: '66,6',
+        id: 1,
+        image: Avatar,
+        name: 'Putin FM',
+    },
+    {
+        frequency: '101,2',
+        id: 2,
+        image: Avatar,
+        name: 'Dribbble FM',
+    },
+    {
+        frequency: '99,4',
+        id: 3,
+        image: Avatar,
+        name: 'Doge FM',
+    },
+    {
+        frequency: '87,1',
+        id: 4,
+        image: Avatar,
+        name: 'Ballads FM',
+    },
+    {
+        frequency: '142,2',
+        id: 5,
+        image: Avatar,
+        name: 'Maximum FM',
+    },
+];
+
+export const FetchStations = async (): Promise<Array<IRadioStation>> => {
+    const r: Array<IRadioStation> = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(mockApiData);
+        }, 300);
+    });
+    return r;
+};
--- src/sagas/RadioWidgetSaga/RadioWidgetSaga.spec.tsx
+++ src/sagas/RadioWidgetSaga/RadioWidgetSaga.spec.tsx
@@ -1,4 +1,5 @@
 import { runSaga } from 'redux-saga';
+import * as API from '../../components/utils/API.util';
 import { getStations } from './RadioWidget.saga';
 
 interface IAction {
@@ -6,13 +7,17 @@ interface IAction {
     payload: any;
 }
 
-test('Saga call api', async () => {
+test('Saga call api correctly', async () => {
     const dipatched: Array<IAction> = [];
+    jest.spyOn(API, 'FetchStations').mockImplementation(() =>
+        Promise.resolve(API.mockApiData)
+    );
     await runSaga(
         {
             dispatch: (action: IAction) => dipatched.push(action),
         },
         getStations
     ).toPromise();
-    console.log('​dipatched', dipatched);
+
+    expect(dipatched[0].payload.length).toEqual(5);
 });
